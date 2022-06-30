72da5a55a7c7bbbc2df6eac186ca4046c3520a50
ps726 <91644659+ps726@users.noreply.github.com>
2022-06-30 20:24:10 +1000

    added nerd emoji thing
    when ever they say something it will repeat their message with a nerd emoji

--- anti ryan bot/bot.py
+++ anti ryan bot/bot.py
@@ -1,21 +1,30 @@
-from subprocess import STARTF_USESHOWWINDOW
+from random import *
 import hikari, lightbulb
 
+#connects bot to discord API
 bot = lightbulb.BotApp(
-    token="Token"
+    token="OTkxNjU3NDE2NzM3NDIzNDAw.G6RL97.p6edhgoPOHeOS3bdydsUgolE0iNoldJMCxyyac"
     )
 
+#prints "bot is ready" when bot was connected to all shards and is ready for use
 @bot.listen()
 async def on_bot_started(event: hikari.StartedEvent):
     print("Bot is ready.")
+#sets bots status
     await bot.update_presence(status=hikari.Status.IDLE, activity=hikari.Activity(name="ur mum lmao"))
     
 @bot.listen()
 async def on_message(event: hikari.MessageCreateEvent):
-    if str(event.author_id) == "829239020114280449":
-        await event.message.respond("https://media.discordapp.net/attachments/820047473392877591/990479302963372042/rearendFDVIKFDOFOF.gif")
-    else:
-        print("told ryan to shut up lol")
+    if str(event.author_id) == "695817237780627506":
+        var = randrange(1,3)
+        if var == 1:
+            await event.message.respond("https://media.discordapp.net/attachments/820047473392877591/990479302963372042/rearendFDVIKFDOFOF.gif")
+            print(event.content)
+        elif var == 2:
+            await event.message.respond(f'"{event.content}" - :nerd:')
+            print(event.content)
+        else:
+            pass
 
 
 bot.run(
